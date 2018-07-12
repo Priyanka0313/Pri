795df4ff91f26e6387c1b44dc0f6e55de90959cf
Priyanka0313 <41142482+Priyanka0313@users.noreply.github.com>
2018-07-12 14:36:59 +0530

    Update and rename odd.py to vowels.py

--- odd.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-if(n<0):
-   print("Invalid")
-elif(n%2==1):
-   print("Odd")
-else:
-   print("Even")
-
-  
---
+++ vowels.py
@@ -0,0 +1,8 @@
+a=input()
+if(ord(a)>=32)&(ord(a)<=47)::
+      print("Invalid")
+if a in ('a','e','i','o','u'):
+   print("Vowel")
+else:
+   print("Consonant")
+    
